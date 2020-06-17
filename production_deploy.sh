f509f8c92a985e95d23f4f727908fc94199ced9e
edward <son@binhsonnguyen.com>
2020-06-17 16:20:40 +0700

    remove -it option in deploy script

--- production_deploy.sh
+++ production_deploy.sh
@@ -3,6 +3,6 @@
 cd /home/ubuntu/wordpress/wp-reflectar-theme || exit
 git pull
 cd src || exit
-docker run --rm -it -v "$PWD":/app -w /app composer install
-docker run --rm -it -v "$PWD":/app -w /app node:lts npm install
-docker run --rm -it -v "$PWD":/app -w /app node:lts npm run compile:css
+docker run --rm -v "$PWD":/app -w /app composer install
+docker run --rm -v "$PWD":/app -w /app node:lts npm install
+docker run --rm -v "$PWD":/app -w /app node:lts npm run compile:css
