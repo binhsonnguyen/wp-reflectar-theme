6f66b1ebfd4e30d4af3dbbe939798a0516ce9ae0
edward <son@binhsonnguyen.com>
2020-06-17 16:16:53 +0700

    add deploy scripts

--- production_deploy.sh
+++ production_deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-touch $WORDPRESS_DIR/touched
+cd /home/ubuntu/wordpress/wp-reflectar-theme || exit
+git pull
+cd src || exit
+docker run --rm -it -v "$PWD":/app -w /app composer install
+docker run --rm -it -v "$PWD":/app -w /app node:lts npm install
+docker run --rm -it -v "$PWD":/app -w /app node:lts npm run compile:css
