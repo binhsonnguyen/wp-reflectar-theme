5642fd985eca600dc2d1b53e2b77095c7427cee2
edward <son@binhsonnguyen.com>
2020-06-18 18:25:06 +0700

    update deployment script add command to compile rtl css

--- production_deploy.sh
+++ production_deploy.sh
@@ -3,6 +3,7 @@
 cd /home/ubuntu/wordpress/wp-reflectar-theme || exit
 git pull
 cd src || exit
-docker run --rm -v "$PWD":/app -w /app composer install
 docker run --rm -v "$PWD":/app -w /app node:lts npm install
 docker run --rm -v "$PWD":/app -w /app node:lts npm run compile:css
+docker run --rm -v "$PWD":/app -w /app node:lts npm run compile:rtl
+docker run --rm -v "$PWD":/app -w /app composer install
