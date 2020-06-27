8d08a698f7e7f6544ba3ecb351020b31b2322b06
edward <son@binhsonnguyen.com>
2020-06-27 16:55:00 +0700

    remove unused build command from deployment

--- production_deploy.sh
+++ production_deploy.sh
@@ -6,7 +6,3 @@ cd /home/ubuntu/wordpress/wp-reflectar-theme
 git checkout master
 git fetch --all
 git reset --hard origin/master
-docker run --rm -v "$PWD"/src:/app -w /app node:lts npm install
-docker run --rm -v "$PWD"/src:/app -w /app node:lts npm run compile:css
-docker run --rm -v "$PWD"/src:/app -w /app node:lts npm run compile:rtl
-docker run --rm -v "$PWD"/src:/app -w /app composer install
