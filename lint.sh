8d4fdb83221cce2a15df9fea4f0660300037af09
edward <son@binhsonnguyen.com>
2020-06-28 14:27:17 +0700

    remove list script and READM.txt

--- lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-docker run --rm -v "$PWD"/src:/app -w /app node:lts npm run lint:scss
-docker run --rm -v "$PWD"/src:/app -w /app node:lts npm run lint:js
-docker run --rm -v "$PWD"/src:/app -w /app composer lint:wpcs
-docker run --rm -v "$PWD"/src:/app -w /app composer lint:php
