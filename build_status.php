4222a20471bd6f287c3b9822f51915b9eca844f8
edward <son@binhsonnguyen.com>
2020-07-13 00:37:18 +0700

    update badge icon

--- build_status.php
+++ build_status.php
@@ -1,3 +1,3 @@
-<a href="https://github.com/binhsonnguyen/wp-reflectar-theme"><img src="https://badgen.net/badge/reflectar/0.0.1?color=black&icon=github"></a>
-<a href="https://github.com/binhsonnguyen/wp-syntaxism-code-block-plugin"><img src="https://badgen.net/badge/syntaxism-code-block/0.0.1?color=black&icon=github"></a>
+<a href="https://github.com/binhsonnguyen/wp-reflectar-theme"><img src="https://badgen.net/travis/binhsonnguyen/wp-reflectar-theme/master?label=reflectar"></a>
+<a href="https://github.com/binhsonnguyen/wp-syntaxism-code-block-plugin"><img src="https://badgen.net/travis/binhsonnguyen/wp-syntaxism-code-block-plugin/master?label=syntaxism-code-block"></a>
 <a href="https://nguyenbinhson.com/wp-login.php?action=register"><img src="https://badgen.net/badge/registration/ON?color=green"></a>
